ad5dc2dedd1fa4d2601f54f7770d48a7c026ce39
John Handley <jwhandley17@gmail.com>
2025-05-27 17:33:16 +0200

    Factor out context data

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import * as Plot from "@observablehq/plot";
   import PlotComponent from "./lib/Plot.svelte";
   import { onMount } from "svelte";
+  import { parties, qualifications, age, tenure } from "./data";
 
   let isDark = $state(false);
 
@@ -18,7 +19,7 @@
   let current = $state(0);
 
   let selected = $state("parties");
-  const options = ["parties", "qualifications", "age"];
+  const options = ["parties", "qualifications", "age", "tenure"];
 
   let context = $derived.by(() => {
     switch (selected) {
@@ -28,121 +29,12 @@
         return qualifications;
       case "age":
         return age;
+      case "tenure":
+        return tenure;
       default:
         return [];
     }
   });
-
-  const age = [
-    {
-      label: "18-24",
-      leftRight: 2.5261142220012913,
-      authLib: 4.842430590968729,
-      size: 10.533509317553325,
-    },
-    {
-      label: "25-49",
-      leftRight: 2.6358959683886627,
-      authLib: 5.9319533962227045,
-      size: 42.571959611845315,
-    },
-    {
-      label: "50-64",
-      leftRight: 2.944215693004888,
-      authLib: 6.7035508587942845,
-      size: 24.414217120437478,
-    },
-    {
-      label: "65+",
-      leftRight: 3.5271540476409524,
-      authLib: 7.0363964711092235,
-      size: 22.48031395016389,
-    },
-  ];
-
-  const qualifications = [
-    {
-      label: "No qualifications",
-      leftRight: 2.7719985767964204,
-      authLib: 7.3524550109434665,
-      size: 6.1638521143678595,
-    },
-    {
-      label: "Below GCSE",
-      leftRight: 2.9501098127988983,
-      authLib: 7.503285117149322,
-      size: 4.046557454205965,
-    },
-    {
-      label: "GCSE",
-      leftRight: 2.881179777145288,
-      authLib: 7.109789544405479,
-      size: 20.700113002185784,
-    },
-    {
-      label: "A-level",
-      leftRight: 2.8842694876895014,
-      authLib: 6.221112167561891,
-      size: 21.12521304321468,
-    },
-    {
-      label: "Undergraduate",
-      leftRight: 2.8683349607918354,
-      authLib: 5.729038351578414,
-      size: 35.45277378634899,
-    },
-    {
-      label: "Postgrad",
-      leftRight: 2.790564628552675,
-      authLib: 5.012408125193036,
-      size: 12.511490599676712,
-    },
-  ];
-
-  const parties = [
-    {
-      label: "Conservative",
-      leftRight: 4.44,
-      authLib: 7.34,
-      size: 23.7,
-      color: "#0087dc",
-    },
-    {
-      label: "Labour",
-      leftRight: 2.3,
-      authLib: 5.4,
-      size: 33.7,
-      color: "#e4003b",
-    },
-    {
-      label: "Lib Dem",
-      leftRight: 2.78,
-      authLib: 5.34,
-      size: 12.1,
-      color: "#faa619",
-    },
-    {
-      label: "SNP",
-      leftRight: 1.8,
-      authLib: 4.95,
-      size: 2.5,
-      color: "#fdf38e",
-    },
-    {
-      label: "Green",
-      leftRight: 1.79,
-      authLib: 4.38,
-      size: 6.4,
-      color: "#06a95b",
-    },
-    {
-      label: "Reform",
-      leftRight: 3.22,
-      authLib: 7.63,
-      size: 14.3,
-      color: "#12b6cf",
-    },
-  ];
 
   function closestGroup() {
     return context
---
+++ src/data.ts
@@ -0,0 +1,143 @@
+export const age = [
+    {
+        label: "18-24",
+        leftRight: 2.5261142220012913,
+        authLib: 4.842430590968729,
+        size: 10.533509317553325,
+    },
+    {
+        label: "25-49",
+        leftRight: 2.6358959683886627,
+        authLib: 5.9319533962227045,
+        size: 42.571959611845315,
+    },
+    {
+        label: "50-64",
+        leftRight: 2.944215693004888,
+        authLib: 6.7035508587942845,
+        size: 24.414217120437478,
+    },
+    {
+        label: "65+",
+        leftRight: 3.5271540476409524,
+        authLib: 7.0363964711092235,
+        size: 22.48031395016389,
+    },
+];
+
+export const qualifications = [
+    {
+        label: "No qualifications",
+        leftRight: 2.7719985767964204,
+        authLib: 7.3524550109434665,
+        size: 6.1638521143678595,
+    },
+    {
+        label: "Below GCSE",
+        leftRight: 2.9501098127988983,
+        authLib: 7.503285117149322,
+        size: 4.046557454205965,
+    },
+    {
+        label: "GCSE",
+        leftRight: 2.881179777145288,
+        authLib: 7.109789544405479,
+        size: 20.700113002185784,
+    },
+    {
+        label: "A-level",
+        leftRight: 2.8842694876895014,
+        authLib: 6.221112167561891,
+        size: 21.12521304321468,
+    },
+    {
+        label: "Undergraduate",
+        leftRight: 2.8683349607918354,
+        authLib: 5.729038351578414,
+        size: 35.45277378634899,
+    },
+    {
+        label: "Postgrad",
+        leftRight: 2.790564628552675,
+        authLib: 5.012408125193036,
+        size: 12.511490599676712,
+    },
+];
+
+export const parties = [
+    {
+        label: "Conservative",
+        leftRight: 4.44,
+        authLib: 7.34,
+        size: 23.7,
+        color: "#0087dc",
+    },
+    {
+        label: "Labour",
+        leftRight: 2.3,
+        authLib: 5.4,
+        size: 33.7,
+        color: "#e4003b",
+    },
+    {
+        label: "Lib Dem",
+        leftRight: 2.78,
+        authLib: 5.34,
+        size: 12.1,
+        color: "#faa619",
+    },
+    {
+        label: "SNP",
+        leftRight: 1.8,
+        authLib: 4.95,
+        size: 2.5,
+        color: "#fdf38e",
+    },
+    {
+        label: "Green",
+        leftRight: 1.79,
+        authLib: 4.38,
+        size: 6.4,
+        color: "#06a95b",
+    },
+    {
+        label: "Reform",
+        leftRight: 3.22,
+        authLib: 7.63,
+        size: 14.3,
+        color: "#12b6cf",
+    },
+];
+
+export const tenure = [
+    {
+        "label": "Other",
+        "leftRight": 2.491707364993186,
+        "authLib": 5.425490545843357,
+        "size": 14.28391291166805
+    },
+    {
+        "label": "Own outright",
+        "leftRight": 3.4354802236437685,
+        "authLib": 6.795592211810938,
+        "size": 32.63617066858963
+    },
+    {
+        "label": "Own with mortgage",
+        "leftRight": 2.99610381906059,
+        "authLib": 6.142003425753704,
+        "size": 28.41182526471972
+    },
+    {
+        "label": "Private rent",
+        "leftRight": 2.4061236209381485,
+        "authLib": 5.6731651143984,
+        "size": 13.901998043675077
+    },
+    {
+        "label": "Social rent",
+        "leftRight": 2.2041677420496035,
+        "authLib": 6.852455454888624,
+        "size": 10.766093111347514
+    }
+];
